6b146cb51d85af206d348216e2f0594c84e64277
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2024-07-01 09:38:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import streamlit as st
 from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link, get_geolocation
 import json
+import pandas as pd
 
 if st.checkbox("Check my location"):
     loc = get_geolocation()
     st.write(f"Your coordinates are {loc}")
+
+
+df = pd.DataFrame(loc)
+
+st.dataframe(df)
+
+
+#st.map(
     
